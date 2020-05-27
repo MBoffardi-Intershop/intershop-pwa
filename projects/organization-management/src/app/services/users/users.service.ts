f0a58e7beb0368d5fe6c0859716a679f5f144a53
Sebastian Hähnlein <S.Haehnlein@intershop.de>
2020-05-27 10:22:42 +0200

    feat: user management - delete user

--- projects/organization-management/src/app/services/users/users.service.ts
+++ projects/organization-management/src/app/services/users/users.service.ts
@@ -1,9 +1,12 @@
 import { Injectable } from '@angular/core';
+import { Observable, throwError } from 'rxjs';
 import { map } from 'rxjs/operators';
 
+import { CustomerUserType } from 'ish-core/models/customer/customer.model';
 import { Link } from 'ish-core/models/link/link.model';
 import { UserData } from 'ish-core/models/user/user.interface';
 import { UserMapper } from 'ish-core/models/user/user.mapper';
+import { User } from 'ish-core/models/user/user.model';
 import { ApiService, resolveLinks, unpackEnvelope } from 'ish-core/services/api/api.service';
 
 @Injectable({ providedIn: 'root' })
@@ -16,5 +19,59 @@ export class UsersService {
       resolveLinks<UserData>(this.apiService),
       map(users => users.map(UserMapper.fromData))
     );
+  }
+
+  deleteUser(customerItemUserKey: string) {
+    if (!customerItemUserKey) {
+      return throwError('deleteUser() called without customerItemUserKey');
+    }
+
+    return this.apiService.delete(`customers/-/users/${customerItemUserKey}`);
+  }
+
+  addUser(body: CustomerUserType): Observable<User> {
+    if (!body || !body.customer || !body.user) {
+      return throwError('addUser() called without required body data');
+    }
+
+    if (!body.customer.type) {
+      return throwError('addUser() called without required customer type (PrivateCustomer/SMBCustomer)');
+    }
+
+    return this.apiService
+      .post<User>('customers/-/users', {
+        ...body.customer,
+        ...body.user,
+        preferredInvoiceToAddress: { urn: body.user.preferredInvoiceToAddressUrn },
+        preferredShipToAddress: { urn: body.user.preferredShipToAddressUrn },
+        preferredPaymentInstrument: { id: body.user.preferredPaymentInstrumentId },
+        preferredInvoiceToAddressUrn: undefined,
+        preferredShipToAddressUrn: undefined,
+        preferredPaymentInstrumentId: undefined,
+      })
+      .pipe(map(UserMapper.fromData));
+  }
+
+  updateUser(customerItemUserKey: string, body: CustomerUserType): Observable<User> {
+    if (!body || !body.customer || !body.user) {
+      return throwError('updateUser() called without required body data');
+    }
+
+    if (!body.customer.type) {
+      return throwError('updateUser() called without required customer type (PrivateCustomer/SMBCustomer)');
+    }
+
+    return this.apiService
+      .put<User>(`customers/-/users/${customerItemUserKey}`, {
+        ...body.customer,
+        ...body.user,
+        preferredInvoiceToAddress: { urn: body.user.preferredInvoiceToAddressUrn },
+        preferredShipToAddress: { urn: body.user.preferredShipToAddressUrn },
+        preferredPaymentInstrument: { id: body.user.preferredPaymentInstrumentId },
+        preferredInvoiceToAddressUrn: undefined,
+        preferredShipToAddressUrn: undefined,
+        preferredPaymentInstrumentId: undefined,
+      })
+      .pipe(map(UserMapper.fromData));
   }
 }
